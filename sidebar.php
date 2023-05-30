c09865e3d8ba3ae62647054c13655a7ee81b2eda
yunitandzz <yunitawulandari747@gmail.com>
2023-05-30 15:32:01 +0800

    inisial commit

---
+++ sidebar.php
@@ -0,0 +1,50 @@
+<!-- sidebar menu area start -->
+<div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.html"><img src="assets/images/icon/logo.png" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+
+                            <li class="active">
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>DASHBOARD</span></a>
+                                
+                            </li>
+
+                            <li class="active">
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>MASTER DATA</span></a>
+                                <ul class="collapse">
+                                    <li class="active"><a href="./pemasok/pemasok.php">PEMASOK</a></li>
+                                    <li><a href="index2.html">BARANG</a></li>
+                                    <li><a href="index3.html">KATEGORI</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>TRANSAKSI
+                                    </span></a>
+                                <ul class="collapse">
+                                    <li><a href="index.html">PEMBELIAN</a></li>
+                                    <li><a href="index3-horizontalmenu.html">PENJUALAN</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-pie-chart"></i><span>LAPORAN</span></a>
+                                <ul class="collapse">
+                                    <li><a href="barchart.html">BARANG MASUK</a></li>
+                                    <li><a href="linechart.html">BARANG KELUAR</a></li>
+                                    <li><a href="piechart.html">KARTU PERSEDIAAN</a></li>
+                                </ul>
+                            </li>
+                           
+                            
+                           
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
